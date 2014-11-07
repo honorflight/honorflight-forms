ec43eba0887c9693cca7bbaa894f1e07ef662848
Jeff Ancel <jancel@gmail.com>
2014-11-07 09:51:59 -0600

    adding Salesforce plugin to this plugin

---
+++ honorflight-forms-plugin.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: HonorFlight Forms Plugin
+Plugin URI: http://gslhonorflight.org
+Description: The HonorFlight Forms plugin is a custom plugin for Greater St. Louis Honor Flight integrations
+Version: 0.0
+Author: Students and Co-workers
+Author URI: http://wustl.edu
+License: GPL2
+*/
+
+
+?>
---
+++ honorflight-forms-plugin/options.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+This file holds the administrative functions of this plugin.  Namely, Salesforce user name, 
+password and security token configurations (for storage in database)
+*/
+
+
+?>
