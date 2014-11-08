abda52d1420aaeaa21e188d322990723d8aacc23
Jeff Ancel <jancel@gmail.com>
2014-11-08 12:20:16 -0600

    checkup

--- honorflight-forms-plugin.php
+++ honorflight-forms-plugin.php
@@ -11,18 +11,31 @@ License: GPL2
 
 /*  Admin Menu */
 class options_page {
+
   function __construct() {
     add_action( 'admin_menu', array( $this, 'admin_menu' ) );
   }
+
   function admin_menu () {
     add_options_page( 'Honor Flight Options','Honor Flight Options','manage_options','options_page_slug', array( $this, 'settings_page' ) );
   }
+
   function  settings_page () {
     include 'honorflight-forms-plugin/admin/settings.php';
   }
+  
 }
 new options_page;
 /* End Admin Menu */
+
+/* API Page */
+class api_page {
+  function __construct() {
+
+  }
+}
+
+
 
 
 require_once "api/routes.php";
