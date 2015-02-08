f2e9dc0b17c2b02d265e8cfd357e842e28cbd6b3
Jeff Ancel <jancel@gmail.com>
2015-02-08 13:34:37 -0600

    full round robin demo complete

--- api/routes.php
+++ api/routes.php
@@ -10,14 +10,21 @@
  *   -> https://github.com/codeguy/Slim/
  *   -> https://github.com/Botnary/wp-slim-framework
  */
-require 'force/force.php';
+// require 'force/force.php';
+require 'person-proxy.php';
 
 //replace empty functions with handles
 add_action("slim_mapping", function($app){
 	header("Slim: Remap");
-  $app->get('/api/force/:obj_type', function($object)use($app){
-    query_salesforce($object);
-  });
+  // $app->get('/api/force/:obj_type', function($object)use($app){
+  //   query_salesforce($object);
+  // });
+
+  // /api/person -> function person()
+  $app->post('/api/:form_name', function($method)use($app){
+     //Pass App context to routing method
+     $method($app);
+   });
 });
 
 ?>
