e12ae6d6c1dd12cbcfaa92943e2c1384ddb3f331
Rob Brockschmidt <generelspaz@gmail.com>
2015-03-02 23:06:48 -0600

    Added medical condition accordion group static

--- forms/js/medical_condition_controller.js
+++ forms/js/medical_condition_controller.js
@@ -17,3 +17,23 @@ function MedicalConditionController($log, $state, medicalConditionTypes, medical
     $log.debug("MedicalConditionController::End");
 }
 angular.module('hf').controller('MedicalConditionController', MedicalConditionController);
+
+angular.module('ui.bootstrap').controller('AccordionDemoCtrl', function ($scope) {
+  $scope.oneAtATime = true;
+
+  $scope.groups = [
+  {
+    title: 'Dynamic Group Header - 1',
+    content: 'Dynamic Group Body - 1'
+  },
+  {
+    title: 'Dynamic Group Header - 2',
+    content: 'Dynamic Group Body - 2'
+  }
+  ];
+
+  $scope.status = {
+    isFirstOpen: true,
+    isFirstDisabled: false
+  };
+});
